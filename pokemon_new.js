66d3c7d4be307287936c80be4bf436b09d4c7ead
Kaden Duncan-Matis <kadenduncanmatis@gmail.com>
2023-10-23 22:59:24 -0600

    Gender info complete

--- pokemon_new.js
+++ pokemon_new.js
@@ -46,44 +46,225 @@ class PokemonGender
         }
     }
 
-    get gender_rate_male() // Returns
+    get gender_rate_male() // Returns null if has no gender.
     {
+        if (!this.has_gender)
+        {
+            return null;
+        }
         return this.gender_rate;
     }
 
-    get gender_rate_female()
+    get gender_rate_female() // Returns null if has no gender.
     {
+        if (!this.has_gender)
+        {
+            return null;
+        }
         return 1 - (this.gender_rate);
     }
 
-    get gender_rate_male_percent() // Returns
+    get gender_rate_male_percent() // Returns null if has no gender.
     {
+        if (!this.has_gender)
+        {
+            return null;
+        }
         return this.gender_rate * 100;
     }
 
-    get gender_rate_female_percent()
+    get gender_rate_female_percent() // Returns null if has no gender.
     {
+        if (!this.has_gender)
+        {
+            return null;
+        }
         return 100 - (this.gender_rate * 100) ;
     }
 
-    get gender_rate()
+    get gender_rate() // Returns null if has no gender.
     {
+        if (!this.has_gender)
+        {
+            return null;
+        }
         return {"m": this.gender_rate_male, "f": this.gender_rate_female};
     }
 
     get gender_fact_easy() // Returns a fact string
     {
-
+        let female_chance = this.gender_rate * 100;
+        let male_chance = 100 - female_chance;
+        if (!this.has_gender)
+        {
+            return "This Pokémon does not have a gender."
+        }
+        switch(female_chance)
+                {
+                    case 100:
+                        if (Math.floor(Math.random() * 2) === 0) // We use some randomness here to vary the output.
+                        {
+                            return "This Pokémon can never be male.";
+                        }
+                        else
+                        {
+                            return "This Pokémon is always female.";
+                        }
+                        break;
+                    case 0:
+                        if (Math.floor(Math.random() * 2) === 0) // We use some randomness here to vary the output.
+                        {
+                            return "This Pokémon can never be female.";
+                        }
+                        else
+                        {
+                            return "This Pokémon is always male.";
+                        }
+                        break;
+                    default:
+                        if (Math.floor(Math.random() * 2) === 0) // We use some randomness here to vary the output.
+                        {
+                            return "This Pokémon has a " + String(Math.trunc(male_chance)) + "% chance of being male and a " + String(Math.trunc(female_chance)) + "% chance of being female.";
+                        }
+                        else
+                        {
+                            return "This Pokémon has a " + String(Math.trunc(female_chance)) + "% chance of being female and a " + String(Math.trunc(male_chance)) + "% chance of being male.";
+                        }
+                        break;
+                }
     }
 
-    get gender_fact_medium()
+    get gender_fact_medium() // Returns a fact string
     {
-
+        let female_chance = this.gender_rate * 100;
+        let male_chance = 100 - female_chance;
+        if (!this.has_gender)
+        {
+            return "This Pokémon does not have a gender."
+        }
+        switch(Math.floor(Math.random() * 4))
+        {
+            case 0:
+                switch(this.has_gender_differences)
+                {
+                    case true:
+                        return "This Pokémon has visible gender differences.";
+                        break;
+                    case false:
+                        return "This Pokémon has no visible gender differences.";
+                }
+                break;
+            default:
+                switch(female_chance)
+                {
+                    case 100:
+                        if (Math.floor(Math.random() * 2) === 0) // We use some randomness here to vary the output.
+                        {
+                            return "This Pokémon can never be male.";
+                        }
+                        else
+                        {
+                            return "This Pokémon is always female.";
+                        }
+                        break;
+                    case 0:
+                        if (Math.floor(Math.random() * 2) === 0) // We use some randomness here to vary the output.
+                        {
+                            return "This Pokémon can never be female.";
+                        }
+                        else
+                        {
+                            return "This Pokémon is always male.";
+                        }
+                        break;
+                    default:
+                        if (Math.floor(Math.random() * 2) === 0) // We use some randomness here to vary the output.
+                        {
+                            return "This Pokémon has a " + String(Math.trunc(male_chance)) + "% chance of being male and a " + String(Math.trunc(female_chance)) + "% chance of being female.";
+                        }
+                        else
+                        {
+                            return "This Pokémon has a " + String(Math.trunc(female_chance)) + "% chance of being female and a " + String(Math.trunc(male_chance)) + "% chance of being male.";
+                        }
+                        break;
+                }
+                break;
+        }
+        
     }
 
-    get gender_fact_hard()
+    get gender_fact_hard() // Returns a fact string
     {
-
+        let female_chance = this.gender_rate * 100;
+        let male_chance = 100 - female_chance;
+        if (!this.has_gender)
+        {
+            switch(Math.floor(Math.random() * 2))
+            {
+                case 0:
+                    return "This Pokémon does not have a gender."
+                    break;
+                case 1:
+                    return "This Pokémon does not have visible gender differences.";
+                    break;
+                default:
+                    throw new Error("Invalid Random Number (Not between 0 and 1).");
+            }
+        }
+        switch(Math.floor(Math.random() * 2))
+        {
+            case 0:
+                switch(this.has_gender_differences)
+                {
+                    case true:
+                        return "This Pokémon has visible gender differences.";
+                        break;
+                    case false:
+                        return "This Pokémon has no visible gender differences.";
+                }
+                break;
+            case 1:
+                switch(female_chance)
+                {
+                    case 100:
+                        if (Math.floor(Math.random() * 2) === 0) // We use some randomness here to vary the output.
+                        {
+                            return "This Pokémon can never be male.";
+                        }
+                        else
+                        {
+                            return "This Pokémon is always female.";
+                        }
+                        break;
+                    case 0:
+                        if (Math.floor(Math.random() * 2) === 0) // We use some randomness here to vary the output.
+                        {
+                            return "This Pokémon can never be female.";
+                        }
+                        else
+                        {
+                            return "This Pokémon is always male.";
+                        }
+                        break;
+                    default:
+                        if (this.gender_rate_female === this.gender_rate_male)
+                        {
+                            return "This Pokémon has an equal chance to be either male or female.";
+                        }
+                        else if (this.gender_rate_female > this.gender_rate_male)
+                        {
+                            return "This Pokémon is more likely to be female than male.";
+                        }
+                        else
+                        {
+                            return "This Pokémon is more likely to be male than female."
+                        }
+                        break;
+                }
+                break;
+            default:
+                throw new Error("Invalid Random Number (Not between 0 and 1).");
+        }
     }
 
     get_gender_fact(difficulty)
